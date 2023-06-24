1609001b9184a2d163fe0828e22802d90d29176d
Derek Worthen <worthend.derek@gmail.com>
2023-06-24 14:44:35 -0700

    support both bash and pwsh install scripts

--- generators/github.go
+++ generators/github.go
@@ -14,13 +14,126 @@ import (
 )
 
 //go:embed templates/github/bash-install.go.tmpl
-var ghTemplate string
+var bashTemplate string
+
+//go:embed templates/github/powershell-install.go.tmpl
+var pwshTemplate string
+
+type PromptVariables struct {
+	RepoUrl             string
+	ArchiveName         string
+	LinuxPlatformDict   string
+	LinuxArchDict       string
+	WindowsPlatformDict string
+	WindowsArchDict     string
+	Destination         string
+}
+
+func NewPromptVariables() *PromptVariables {
+	variables := new(PromptVariables)
+	variables.RepoUrl = promptMessage("Provide the GitHub project in the format USERNAME/REPO", "", true)
+	printSpacer()
+	variables.ArchiveName = promptMessage(strings.TrimSpace(`
+Provide the archive template name. The template may make use of the following variables:
+
+  {PLATFORM}: The OS of the machine that results from running 'uname -s' on linux/darwin machines
+    and is the literal value 'Windows' on windows.
+  {ARCH}: The architecture of the machine that results from running 'uname -m' on linux/darwin
+    machines and $ENV:PPROCESSOR_ACHITECTURE on windows.
+  {TAG}: The specified GitHub release tag. Defaults to the latest tag if one was not specified
+    when calling the install script.
+  {ARCHIVE_EXT}: An extension specific to the PLATFORM. Defaults to '.tar.gz' on linux/darwin
+    and '.zip' on windows.
+  {BINARY_EXT}: The binary extension specific to the PLATFORM. Defaults to '' on linux/darwin
+    and '.exe' on windows.
+
+Example:
+  myCoolApp_{PLATFORM}_{ARCH}_{TAG}{ARCHIVE_EXT}
+Which might result in
+  myCoolApp_Windows_x86_64_v1.0.5.zip when installing on windows with the produced install.ps1 script
+
+Archive Name
+  `), "", true)
+	printSpacer()
+	variables.LinuxPlatformDict = promptMessage(strings.TrimSpace(`
+Provide a comma separated list of KEY=VALUE Platform mapping to use on Linux/Darwin machines.
+This mapping helps bridge the gap from what is produced by 'uname -s | cut -d- -f1' and what
+might be listed in GitHub releases. For example, the value produced on the machine might be the OS
+capitalized, e.g., 'Linux' while the value listed in GitHub actions might be with a lowercase value,
+'linux'. Or the project on GitHub might use the value 'mac' in their release names instead of
+the OS value 'Darwin'. In this case, the mapping "Linux=linux,Darwin=mac" would be helpful.
+
+Default: Linux=Linux,Darwin=Darwin
+
+Linux/Darwin Platform Mapping
+`), "Linux=Linux,Darwin=Darwin", false)
+	printSpacer()
+	variables.LinuxArchDict = promptMessage(strings.TrimSpace(`
+Provide a comma separated list of KEY=VALUE architecture mapping for Linux/Darwin platforms.
+Similar to the Platform mapping, this helps bridge the gap of what is produced on the machine
+when the install script runs and the value that is used on GitHub releases. For example, the
+machine might list the architecture 'x86_64' when running 'uname -m' but the GitHub project
+might be using the value 'amd64' in their release names. In that case the mapping x86_64=amd64
+would be helpful.
+
+Default: x86_64=x86_64
+
+Linux/Darwin Architecture Mapping
+  `), "x86_64=x86_64", false)
+	printSpacer()
+	variables.WindowsPlatformDict = promptMessage(strings.TrimSpace(`
+Provide a comma separated list of possible KEY=VALUE platform mappings to use on Windows.
+
+Default: Windows=Windows
+
+Windows Platform Mapping
+  `), "Windows=Windows", false)
+	printSpacer()
+	variables.WindowsArchDict = promptMessage(strings.TrimSpace(`
+Provide a comma separated list of possible KEY=VALUE architecture mappings to use on Windows.
+The key is derived from the environment variable PROCESSOR_ARCHITECTURE. By default, we map
+the AMD64 value to x86_64 to be more consistent with what is produced by 'uname -m' on
+Linux/Darwin machines.
+
+Default: AMD64=x86_64
+
+Windows Architecture Mapping
+  `), "AMD64=x86_64", false)
+	printSpacer()
+	variables.Destination = promptMessage("Location to create install scripts (defaults to '.')", ".", false)
+	return variables
+}
+
+func (variables *PromptVariables) Validate() {
+	repoUrlSlashCount := strings.Count(variables.RepoUrl, "/")
+	if repoUrlSlashCount != 1 {
+		fmt.Printf("Error: Expecting GitHub repo project to be in the form USERNAME/REPO but recieved %v", variables.RepoUrl)
+		os.Exit(1)
+	}
+	variables.RepoUrl = fmt.Sprintf("https://github.com/%v", variables.RepoUrl)
+
+	variables.LinuxPlatformDict = mapToBashDict(keyValueToMap(variables.LinuxPlatformDict))
+	variables.LinuxArchDict = mapToBashDict(keyValueToMap(variables.LinuxArchDict))
+	variables.WindowsPlatformDict = mapToPwshDict(keyValueToMap(variables.WindowsPlatformDict))
+	variables.WindowsArchDict = mapToPwshDict(keyValueToMap(variables.WindowsArchDict))
+	dest, err := filepath.Abs(utils.ToFilePath(variables.Destination))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	variables.Destination = dest
+}
 
 func GenerateGithubInstallers() {
-	variables := prompt()
-	shapeVariables(variables)
+	variables := NewPromptVariables()
+	variables.Validate()
 
-	tmpl, err := template.New("bash").Parse(ghTemplate)
+	writeTemplate(variables, bashTemplate, "install.sh")
+	writeTemplate(variables, pwshTemplate, "install.ps1")
+}
+
+func writeTemplate(variables *PromptVariables, templateString string, scriptName string) {
+	tmpl, err := template.New("template").Parse(templateString)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -33,59 +146,26 @@ func GenerateGithubInstallers() {
 		os.Exit(1)
 	}
 
-	dest := filepath.Join(variables["dest"], "install.sh")
+	dest := filepath.Join(variables.Destination, scriptName)
 	os.WriteFile(dest, []byte(buf.String()), 0755)
 }
 
-func prompt() map[string]string {
-	variables := map[string]string{}
-	promptMessage(variables, "repoUrl", "repoUrl", true)
-	promptMessage(variables, "archiveName", "Archive template", true)
-	promptMessage(variables, "platformDict", "Platform map (optional, comma separated key=value. Example: win=Windows)", false)
-	promptMessage(variables, "archDict", "Architecture map (Optional, comma separated key=value. Example: x86_64=amd64)", false)
-	promptMessage(variables, "dest", "Destination directory (Optional, defaults to '.')", false)
-	return variables
-}
-
-func promptMessage(variables map[string]string, key string, message string, required bool) {
+func promptMessage(message string, defaultValue string, required bool) string {
+	var response string
 	for {
-		variables[key] = prompter.Prompt(message, "")
-		if val, ok := variables[key]; !required || (ok && val != "") {
+		response = prompter.Prompt(message, defaultValue)
+		if !required || response != "" {
 			break
 		}
 	}
+	return response
 }
 
-func shapeVariables(variables map[string]string) {
-	var platformMapping map[string]string
-	var archMapping map[string]string
-
-	if val, ok := variables["platformDict"]; ok && val != "" {
-		platformMapping = keyValueToMap(val)
-	} else {
-		platformMapping = getDefaultPlatformMapping()
-	}
-
-	if val, ok := variables["archDict"]; ok && val != "" {
-		archMapping = keyValueToMap(val)
-	} else {
-		archMapping = getDefaultArchMapping()
-	}
-
-	if val, ok := variables["dest"]; !ok || val == "" {
-		variables["dest"] = "."
-	}
-
-	val, err := filepath.Abs(utils.ToFilePath(variables["dest"]))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	variables["dest"] = val
-
-	variables["platformDict"] = mapToBashDict(platformMapping)
-	variables["archDict"] = mapToBashDict(archMapping)
+func printSpacer() {
+	fmt.Println()
+	fmt.Print("=====================================================")
+	fmt.Println()
+	fmt.Println()
 }
 
 func keyValueToMap(keyVals string) map[string]string {
@@ -108,23 +188,18 @@ func keyValueToMap(keyVals string) map[string]string {
 	return mapping
 }
 
-func getDefaultPlatformMapping() map[string]string {
-	return map[string]string{
-		"Linux":  "Linux",
-		"Darwin": "Darwin",
-	}
-}
-
-func getDefaultArchMapping() map[string]string {
-	return map[string]string{
-		"x86_64": "x86_64",
-	}
-}
-
 func mapToBashDict(mapping map[string]string) string {
 	output := ""
 	for key, value := range mapping {
 		output += fmt.Sprintf("\n\t[\"%s\"]=\"%s\"", key, value)
+	}
+	return output
+}
+
+func mapToPwshDict(mapping map[string]string) string {
+	output := ""
+	for key, value := range mapping {
+		output += fmt.Sprintf("\n\t%s = \"%s\"", key, value)
 	}
 	return output
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,17 @@
 package utils
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
 func ToFilePath(str string) string {
-	return filepath.FromSlash(strings.ReplaceAll(str, `\`, `/`))
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("Unable to load user home dir.")
+		os.Exit(1)
+	}
+	return filepath.Clean(strings.ReplaceAll(str, `~`, homeDir))
 }
